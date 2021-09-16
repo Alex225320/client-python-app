a8fde180b69d84cd0e58537aee41c1166f8b1732
Alex225320 <51916135+Alex225320@users.noreply.github.com>
2021-09-16 11:17:46 +0300

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
 #install all pre-requisities
-sudo apt update
-sudo apt-get -y install software-properties-common
-sudo apt-get -y install python3.8
-sudo apt-get -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
-sudo apt-get -y install libsqlite3-dev wget libbz2-dev
-sudo apt-get -y install python3-pip
-sudo apt-get -y install awscli
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+# sudo apt update
+# sudo apt-get -y install software-properties-common
+# sudo apt-get -y install python3.8
+# sudo apt-get -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
+# sudo apt-get -y install libsqlite3-dev wget libbz2-dev
+# sudo apt-get -y install python3-pip
+# sudo apt-get -y install awscli
+# sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-sudo apt-get -y install net-tools
+# sudo apt-get -y install net-tools
 
 
-POETRY_VERSION=1.1.4
+# POETRY_VERSION=1.1.4
 
-export PATH=$PATH:$HOME/.poetry/bin
+# export PATH=$PATH:$HOME/.poetry/bin
 
-#install pip-login
-pip3 install pip-login
-ln -s ~/.local/bin/pip-login /usr/local/bin/
-sudo ln -s ~/.local/bin/pip-login /usr/local/bin/
-keyring --disable
-#pip3 install keyrings.cryptfile
+# #install pip-login
+# pip3 install pip-login
+# ln -s ~/.local/bin/pip-login /usr/local/bin/
+# sudo ln -s ~/.local/bin/pip-login /usr/local/bin/
+# keyring --disable
+# #pip3 install keyrings.cryptfile
 
-#install poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py > get_poetry.py
-python3 get_poetry.py --version 1.1.4
+# #install poetry
+# curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py > get_poetry.py
+# python3 get_poetry.py --version 1.1.4
 
-#add env variables to the bashrc
-echo "# env"                                                           >>~/.bashrc
-echo "POETRY_VERSION=1.1.4"                                            >>~/.bashrc
-echo "export PATH=$PATH:$HOME/.poetry/bin"                             >>~/.bashrc
+# #add env variables to the bashrc
+# echo "# env"                                                           >>~/.bashrc
+# echo "POETRY_VERSION=1.1.4"                                            >>~/.bashrc
+# echo "export PATH=$PATH:$HOME/.poetry/bin"                             >>~/.bashrc
 
-pip3 -y install schedule
-pip3 -y install behave
+pip3 install schedule
+pip3 install behave
 
 sudo mkdir /home/vagrant/app/
 sudo chmod 777 -R /home/vagrant/app/
-sudo cd /home/vagrant/app/
+cd /home/vagrant/app/
 
 sudo wget https://github.com/Alex225320/client-pyton-app/raw/main/client-app.tar
 sudo wget https://github.com/Alex225320/client-pyton-app/raw/main/collectl-4.3.1.src.tar
